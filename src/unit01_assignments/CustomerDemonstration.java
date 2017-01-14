05cb1f28071569d7c2fb3d5908f5947ac2fb27ad
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-01-14 17:46:16 -0600

    Commit.

---
+++ src/unit01_assignments/CustomerDemonstration.java
@@ -0,0 +1,15 @@
+package unit01_assignments;
+
+public class CustomerDemonstration {
+    
+        public static void main(String[] args) 
+    {
+        Customer customer = new Customer("987-6543", false);
+        
+        
+    }
+
+    
+    
+    
+}
