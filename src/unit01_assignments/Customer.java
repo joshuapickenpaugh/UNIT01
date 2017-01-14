474369c778040850e2d203f4a7d2f4dfebf91643
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-01-14 17:35:35 -0600

    Commit.

--- src/unit01_assignments/Customer.java
+++ src/unit01_assignments/Customer.java
@@ -1,8 +1,55 @@
  package unit01_assignments;
 
 
-public class Customer extends Person {
+public class Customer extends Person {    
     
+    public String custNumber;
+    public Boolean mailingList = false;
     
+    public Customer(String cstnum, Boolean mlnglst)
+    {
+        super("Joe", "Smith", "123 Main", "123-4567");
+        custNumber = cstnum;
+        mailingList = mlnglst;
+    }
+    
+    @Override
+    public String getFirstName()
+    {
+        return fName;
+    }
+    
+    @Override
+    public String getLastName()
+    {
+        return lName;
+    }
+    
+    @Override
+    public String getStreetAddress()
+    {
+        return streetAddress;
+    }
+    
+    @Override
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+    
+    public String getCustNumber()
+    {
+        return custNumber;
+    }
+    
+    public Boolean subscribedToMailingList()
+    {
+        if (mailingList = false)
+        {
+            return false;
+        }
+        else
+            return true;
+    }
     
 }
