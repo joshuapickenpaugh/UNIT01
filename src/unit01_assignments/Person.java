fd5928b1abc65846370b610d978a5eaff249e0c9
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-01-14 16:23:51 -0600

    Commit.

---
+++ src/unit01_assignments/Person.java
@@ -0,0 +1,26 @@
+package unit01_assignments;
+
+
+public class Person 
+{
+    public String fName;
+    public String lName;
+    public String streetAddress;
+    public String phoneNumber;
+    
+    public Person(String fnme, String lnme, String stadd, String phnmbr)
+    {
+        fName = fnme;
+        lName = lnme;
+        streetAddress = stadd;
+        phoneNumber = phnmbr;
+    }
+    
+    public String getFirstName()
+    {
+        return fName;
+    }
+    
+    public 
+    
+}
---
+++ src/unit01_assignments/UNIT01_Assignments.java
@@ -0,0 +1,16 @@
+/*
+Joshua Pickenpaugh
+UNIT 1 Assignments
+1/13/2017
+ */
+package unit01_assignments;
+ 
+public class UNIT01_Assignments {
+ 
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+ 
