942b62d030f665ad619f29d7ffe0bf38161f95a0
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-01-14 18:17:24 -0600

    Commit.

--- src/unit01_assignments/CustomerDemonstration.java
+++ src/unit01_assignments/CustomerDemonstration.java
@@ -6,6 +6,13 @@ public class CustomerDemonstration {
     {
         Customer customer = new Customer("987-6543", false);
         
+        System.out.println(customer.getFirstName());
+        System.out.println(customer.getLastName());
+        System.out.println(customer.getStreetAddress());
+        System.out.println(customer.getPhoneNumber());
+        System.out.println(customer.getCustNumber());
+        System.out.println(customer.subscribedToMailingList());
+                
         
     }
 
