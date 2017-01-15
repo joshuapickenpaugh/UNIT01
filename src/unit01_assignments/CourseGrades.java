0b70861df152c8afec085215789ba5b981f41eb9
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-01-15 10:42:45 -0600

    Commit.

--- src/unit01_assignments/CourseGrades.java
+++ src/unit01_assignments/CourseGrades.java
@@ -1,30 +1,53 @@
 package unit01_assignments;
 
-public class CourseGrades extends GradedActivity
-{
-    public void setLab()
+public class CourseGrades extends GradedActivity //implements Analyzable
+{    
+    //GradedActivity object array:
+    //Idea via JD and SS in group:
+    public GradedActivity[] grades = new GradedActivity[4];  
+    
+    public void setLab(GradedActivity object)
     {
         
     }
     
-    public void setPassFailExam()
+    public void setPassFailExam(GradedActivity object)
     {
         
     }
     
-    public void setEssay()
+    public void setEssay(GradedActivity object)
     {
         
     }
     
-    public void setFinalExam()
+    public void setFinalExam(GradedActivity object)
     {
         
     }
+    
+//    public double getAverage(GradedActivity object)
+//    {
+//        double average = 0;
+//        
+//        return average;
+//    }
+//    public GradedActivity getHighest(GradedActivity object)
+//    {
+//        double highest = 0;
+//        
+//        return GradeActivity;
+//    }
+//    public GradedActivity getLowest(GradedActivity object)
+//    {
+//        double lowest = 0;
+//        
+//        return GradeActivity;
+//    }
     
     @Override
     public String toString()
     {
-        return "stuff goes here";
+        return "(results of test scores go here)";
     }
 }
